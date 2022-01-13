c34eaca8aa80c13f23922c66120508989d0f726a
unknown <tontanit@gmail.com>
2022-01-13 12:11:17 +0700

    add file cquan

--- frontend/header.php
+++ frontend/header.php
@@ -1,23 +1,15 @@
-<?php
-require_once('./database/dbconfig.php');
-?>
 <!DOCTYPE html>
-<html lang="en">
+<!-- Created by CodingLab |www.youtube.com/c/CodingLabYT-->
+<html lang="en" dir="ltr">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="https://gokisoft.com/uploads/2021/03/s-568-ico-web.jpg" />
-
+    <title> Responsive Sidebar Menu | CodingLab </title>
+    <link rel="stylesheet" href="../layout/style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet/less" type="text/css" href="./css/styles.less" />
-    <script src="https://cdn.jsdelivr.net/npm/less@4.1.1"></script>
-    <link rel="stylesheet" href="./css/reset.css">
-    <title>QUAN LY NHAN SU</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-    <div class="grid-container">
-        <div class="header">header</div>
--- nhansu/index.php
+++ nhansu/index.php
@@ -1,123 +1,100 @@
 <?php
 require_once('../database/dbconfig.php');
+require_once('../frontend/header.php');
+require_once('../frontend/nav.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<section class="home-section">
+    <!-- Noi dung web -->
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="https://gokisoft.com/uploads/2021/03/s-568-ico-web.jpg" />
+    <div class="main">
+        <div class="container">
+            <h4>DANH SÁCH CÁN BỘ, CÔNG CHỨC THUỘC DIỆN BTV QUẢN LÝ</h4>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/style.css">
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <title>QUAN LY NHAN SU</title>
-</head>
-
-<body>
-    <div class="grid-container">
-        <div class="header">header</div>
-        <div class="nav">
-            <!-- Noi dung nav -->
-
-
-            <!-- Noi dung nav -->
-        </div>
-        <div class="main">
-            <div class="container">
-                <h4>DANH SÁCH CÁN BỘ, CÔNG CHỨC THUỘC DIỆN BTV QUẢN LÝ</h4>
-
-                <div class="row">
-                    <div class="col-lg-6">
-                        <a href="../nhansu/add.php"><button type=" button" class="btn btn-success" style="margin-bottom: 10px;">Thêm</button></a>
-                    </div>
-                    <div class="col-lg-6">
-                        <form method="get">
-
-                            <div class=" form-group">
-                                <input type="text" class="form-control" name="search" placeholder="Searching..." id="search">
-                                <input class="btn btn-primary mb-2" type="submit" value="Tìm kiếm">
-                            </div>
-                        </form>
-                    </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <a href="../nhansu/add.php"><button type=" button" class="btn btn-success" style="margin-bottom: 10px;">Thêm</button></a>
                 </div>
-                <table class="table table-hover table-bordered ">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>STT</th>
-                            <th>Họ và tên</th>
-                            <th>Năm sinh</th>
-                            <th>Dân tộc</th>
-                            <th>Tôn giáo</th>
-                            <th>Văn hoá</th>
-                            <th>Trình độ</th>
-                            <th>LLCT</th>
-                            <th>Năm vào đảng</th>
-                            <th>chức vụ</th>
-                            <th>Action</th>
-                            <th>Action</th>
+                <div class="col-lg-6">
+                    <form method="get">
+
+                        <div class=" form-group">
+                            <input type="text" class="form-control" name="search" placeholder="Searching..." id="search">
+                            <input class="btn btn-primary mb-2" type="submit" value="Tìm kiếm">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <table class="table table-hover table-bordered ">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>STT</th>
+                        <th>Họ và tên</th>
+                        <th>Năm sinh</th>
+                        <th>Dân tộc</th>
+                        <th>Tôn giáo</th>
+                        <th>Văn hoá</th>
+                        <th>Trình độ</th>
+                        <th>LLCT</th>
+                        <th>Năm vào đảng</th>
+                        <th>chức vụ</th>
+                        <th>Action</th>
+                        <th>Action</th>
 
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $search = '';
-                        $search = getGet('search');
-                        $sql = "SELECT * FROM nhansu
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $search = '';
+                    $search = getGet('search');
+                    $sql = "SELECT * FROM nhansu
                                 WHERE hoten REGEXP '$search' 
                                 ORDER BY id DESC";
-                        $result = ExcuteResult($sql);
-                        $index = 1;
-                        foreach ($result as $list) {
-                        ?>
-                            <tr>
-                                <td><?php echo $index++ ?></td>
-                                <td style="text-align: left;"><?php echo $list['hoten'] ?></td>
-                                <td><?php echo $list['ngaysinh'] ?></td>
-                                <td><?php echo $list['dantoc'] ?></td>
-                                <td><?php echo $list['tongiao'] ?></td>
-                                <td><?php echo $list['vanhoa'] ?></td>
-                                <td><?php echo $list['trinhdo'] ?></td>
-                                <td><?php echo $list['llct'] ?></td>
-                                <td><?php echo $list['namvaodang'] ?></td>
-                                <td style="width: 30px;"><?php echo $list['chucvu'] ?></td>
-                                <td><a href="../nhansu/sua.php?id=<?php echo $list['id'] ?>"><button type="button" class="btn btn-warning">Sửa</button></a></td>
-                                <td><button type="button" class="btn btn-danger" onclick=deleteItem(<?php echo $list['id'] ?>)>Xoá</button></td>
+                    $result = ExcuteResult($sql);
+                    $index = 1;
+                    foreach ($result as $list) {
+                    ?>
+                        <tr>
+                            <td><?php echo $index++ ?></td>
+                            <td style="text-align: left;"><?php echo $list['hoten'] ?></td>
+                            <td><?php echo $list['ngaysinh'] ?></td>
+                            <td><?php echo $list['dantoc'] ?></td>
+                            <td><?php echo $list['tongiao'] ?></td>
+                            <td><?php echo $list['vanhoa'] ?></td>
+                            <td><?php echo $list['trinhdo'] ?></td>
+                            <td><?php echo $list['llct'] ?></td>
+                            <td><?php echo $list['namvaodang'] ?></td>
+                            <td style="width: 30px;"><?php echo $list['chucvu'] ?></td>
+                            <td><a href="../nhansu/sua.php?id=<?php echo $list['id'] ?>"><button type="button" class="btn btn-warning">Sửa</button></a></td>
+                            <td><button type="button" class="btn btn-danger" onclick=deleteItem(<?php echo $list['id'] ?>)>Xoá</button></td>
 
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
-
-
-
-
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <script type="text/javascript">
+                function deleteItem(id) {
+                    var option = confirm('Ban có chắc muốn xóa danh mục này không?');
+                    if (!option) {
+                        return;
+                    }
+                    console.log(id)
+                    //ajax - lenh post
+                    $.post('ajax.php', {
+                        'id': id,
+                        'action': 'delete'
+                    }, function(data) {
+                        location.reload()
+                    })
+                }
+            </script>
         </div>
 
     </div>
+    <!-- End Noi dung web -->
+</section>
 
-    <script type="text/javascript">
-        function deleteItem(id) {
-            var option = confirm('Ban có chắc muốn xóa danh mục này không?');
-            if (!option) {
-                return;
-            }
-            console.log(id)
-            //ajax - lenh post
-            $.post('ajax.php', {
-                'id': id,
-                'action': 'delete'
-            }, function(data) {
-                location.reload()
-            })
-        }
-    </script>
 
-</body>
-
-</html>
+<?php
+require_once('../frontend/footer.php');
+?>
